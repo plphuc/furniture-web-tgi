c0fa87195af8ec3820f2938634e3520dabe0896f
plphuc <phamluumyphuc@gmail.com>
2024-03-07 21:14:56 +0700

    [add] assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
+
+import Header from 'components/Header/Header';
+import Cover from 'components/Cover/Cover';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <Cover />
+      <div className="contentWrapper blueWrapper">
+        <div className="contentContainer">
+          <div className="utilityItem">
+            <div className="logoItem"></div>
+            <div className="infoItem">
+              <div className="headerItem">Free Delivery</div>
+              <div className="descItem">Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+          <div className="utilityItem">
+            <div className="logoItem"></div>
+            <div className="infoItem">
+              <div className="headerItem">Support 24/7</div>
+              <div className="descItem">Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+          <div className="utilityItem">
+            <div className="logoItem"></div>
+            <div className="infoItem">
+              <div className="headerItem">100% Authentic</div>
+              <div className="descItem">Lorem ipsum dolor sit amet.</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='contentWrapper greyWrapper'>
+      <div className='contentContainer'>
+        <span className='titleContent'>Inspiration Collection</span>
+        <span className='descContent'>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+      </div>
+      </div>
     </div>
   );
 }
